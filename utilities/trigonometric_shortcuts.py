4ac7da8b85d257807de743aba5d96b98aba73b05
aunal16 <aunal16@ku.edu.tr>
2022-01-12 15:04:30 +0300

    function explanations added

--- utilities/trigonometric_shortcuts.py
+++ utilities/trigonometric_shortcuts.py
@@ -2,24 +2,30 @@ import numpy as np
 
 
 def sind(x):
+    # sine in degrees
     return np.sin(x * np.pi / 180)
 
 
 def cosd(x):
+    # cosine in degrees
     return np.cos(x * np.pi / 180)
 
 
 def tand(x):
+    # tangent in degrees
     return np.tan(x * np.pi / 180)
 
 
 def asind(x):
+    # arcsine in degrees
     return np.arcsin(x) * 180 / np.pi
 
 
 def acosd(x):
+    # arccosine in degrees
     return np.arccos(x) * 180 / np.pi
 
 
 def atand(x):
+    # arctangent in degrees
     return np.arctan(x) * 180 / np.pi
